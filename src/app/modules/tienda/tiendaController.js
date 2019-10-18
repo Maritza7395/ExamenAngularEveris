5a72ce4b8201559b78eb246377a9ea9eb3b8c31e
maritza <maritza.loaiza02@inacapmail.cl>
2019-10-18 00:13:17 -0300

    carrito listo

--- src/app/modules/tienda/tiendaController.js
+++ src/app/modules/tienda/tiendaController.js
@@ -2,7 +2,7 @@ app.controller("tiendaController", ["rutas", "Auth", "Producto", "Compra","Sessi
     var ctrl=this;
     ctrl.rutas = rutas;
     ctrl.usuario = Auth.parseToken(Auth.getToken()).email;
-    ctrl.productos = Producto.query();
+    ctrl.productos = Producto.query();   
     ctrl.carrito = [];
     ctrl.getCarrito =  function(){
         if(Object.entries(SessionStorage.getObject('carrito')).length === 0){
@@ -19,12 +19,13 @@ app.controller("tiendaController", ["rutas", "Auth", "Producto", "Compra","Sessi
         else{
             ctrl.carrito = SessionStorage.getObject('carrito');
         }
+
     };
+   
+    
     ctrl.getCarrito();
 
     ctrl.addItem = function(item, index){
-        console.log(item);
-        var precioInit = ctrl.productos[index].precio;
         if(Object.entries(ctrl.carrito.items).length === 0){
             item.cantidad = ctrl.cantidad[index];
             item.precio = item.cantidad*item.precio;
@@ -36,12 +37,19 @@ app.controller("tiendaController", ["rutas", "Auth", "Producto", "Compra","Sessi
             for(i=0;i<ctrl.carrito.items.length;i++){
                 
                 if(ctrl.carrito.items[i].id==item.id){
-                    var setCantidad = ctrl.carrito.items[i].cantidad+ctrl.cantidad[index];
-                    ctrl.carrito.items[i].cantidad = setCantidad;
-                    ctrl.carrito.items[i].precio = precioInit * setCantidad;
+                    var setCantidadA = ctrl.carrito.items[i].cantidad;
+                    var moreCantidad = ctrl.cantidad[index];
+                    var precioProductoT = ctrl.productos[i]['precio'];                   
+                   
+                    var precioProducto = precioProductoT/setCantidadA;
+                    var setPrecio = precioProducto * (setCantidadA+moreCantidad);
+                    ctrl.carrito.items[i].precio = setPrecio;
+
+                    ctrl.carrito.items[i].cantidad = setCantidadA+moreCantidad;
                     ItemExist = true;
-                    
+                    console.log(ctrl.carrito.items[i].precio);
                 }
+                
             }
             if(ItemExist==false){
                 item.cantidad = ctrl.cantidad[index];
@@ -51,6 +59,7 @@ app.controller("tiendaController", ["rutas", "Auth", "Producto", "Compra","Sessi
             SessionStorage.setObject('carrito', ctrl.carrito);
         }
         ctrl.carrito.cantidadTotal = ctrl.carrito.cantidadTotal+ctrl.cantidad[index];
+        ctrl.carrito.precioTotal = 0;
         for (var i = 0; i <ctrl.carrito.items.length ; i++){
             ctrl.carrito.precioTotal += ctrl.carrito.items[i].precio;
         };
@@ -58,9 +67,11 @@ app.controller("tiendaController", ["rutas", "Auth", "Producto", "Compra","Sessi
     };
     ctrl.remove=function(item){
         var index = ctrl.carrito.items.indexOf(item);
+        console.log(item);
         ctrl.carrito.items.splice(index, 1);
-        ctrl.carrito.cantidadTotal = ctrl.carrito.cantidadTotal-ctrl.cantidad[index];
-        ctrl.carrito.precioTotal = ctrl.carrito.precioTotal-ctrl.carrito.items[index].precio;
+
+        ctrl.carrito.cantidadTotal = ctrl.carrito.cantidadTotal-item.cantidad;
+        ctrl.carrito.precioTotal = ctrl.carrito.precioTotal-item.precio;
         SessionStorage.setObject('carrito', ctrl.carrito);
     };
 
